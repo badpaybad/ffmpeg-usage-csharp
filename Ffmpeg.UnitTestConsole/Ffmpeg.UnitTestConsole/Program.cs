c24984575e7e781c77b26b06a55872ea48fefbf5
nguyen phan du <badpaybad@gmail.com>
2020-06-03 08:06:25 +0700

    ffmpeg/bin/bat1.bat

--- Ffmpeg.UnitTestConsole/Ffmpeg.UnitTestConsole/Program.cs
+++ Ffmpeg.UnitTestConsole/Ffmpeg.UnitTestConsole/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Ffmpeg.Core;
+using System;
 
 namespace Ffmpeg.UnitTestConsole
 {
@@ -7,6 +8,19 @@ namespace Ffmpeg.UnitTestConsole
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+           var temp= new FfmpegCommandRunner().Run();
+          
+            while (true)
+            {
+                Console.WriteLine("Type quit to exist");
+                var cmd = Console.ReadLine();
+                if (cmd == "quit")
+                {
+                    Environment.Exit(0);
+                }
+            }
+            
         }
     }
 }
