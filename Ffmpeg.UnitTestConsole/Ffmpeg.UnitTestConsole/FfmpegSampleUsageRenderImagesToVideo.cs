96a86dc41a22e348680981ad7df1e4deafe4218c
dunp <dunp@omt.vn>
2020-06-04 10:09:39 +0700

    FfmpegSampleUsageRenderImagesToVideo

---
+++ Ffmpeg.UnitTestConsole/Ffmpeg.UnitTestConsole/FfmpegSampleUsageRenderImagesToVideo.cs
@@ -0,0 +1,64 @@
+﻿using Ffmpeg.Core;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Ffmpeg.UnitTestConsole
+{
+    public class FfmpegSampleUsageRenderImagesToVideo
+    {
+        Random _rnd = new Random();
+        public FfmpegSampleUsageRenderImagesToVideo()
+        {
+
+        }
+        public List<string> ListImageFile()
+        {
+            return Directory.GetFiles(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "ImageTest/imgs"))
+                .Select(i => i).ToList();
+        }
+
+        public List<string> ListAudioFile()
+        {
+            return Directory.GetFiles(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "ImageTest/audio"))
+                .Select(i => i).ToList();
+        }
+
+        public SampleResult Convert()
+        {
+            List<string> audios = ListAudioFile();
+            string audioFile = audios[_rnd.Next(0, audios.Count - 1)];
+
+            var dir = Path.Combine(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "ImageTest/results"));
+
+            if (!Directory.Exists(dir)) Directory.CreateDirectory(dir);
+
+            string fileOutput = Path.Combine(dir, $"video_{DateTime.Now.Ticks}.mp4");
+
+            var cmd = new FFmpegCommandBuilder().WithFileAudio(audioFile)
+                .AddFileInput(ListImageFile().Take(30).ToArray())
+                .WithFileOutput(fileOutput)
+                .WithVideoDurationInSeconds((decimal)3.5*60)
+                .WithFadeDurationInSeconds(2)
+                .ToCommand();
+
+            Console.WriteLine(fileOutput);
+
+            var r = new FfmpegCommandExecuter().Run(cmd);
+
+            return new SampleResult
+            {
+                ConvertResult = r,
+                FileVideo = fileOutput
+            };
+        }
+
+        public class SampleResult
+        {
+            public string FileVideo { get; set; }
+
+            public FfmpegCommandResult ConvertResult { get; set; }
+        }
+    }
+}
--- Ffmpeg.UnitTestConsole/Ffmpeg.UnitTestConsole/Program.cs
+++ Ffmpeg.UnitTestConsole/Ffmpeg.UnitTestConsole/Program.cs
@@ -1,8 +1,6 @@
-﻿using Ffmpeg.Core;
-using Newtonsoft.Json;
+﻿using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using System.Text.Json.Serialization;
 using System.Threading.Tasks;
@@ -15,14 +13,14 @@ namespace Ffmpeg.UnitTestConsole
         {
             Task.Run(async () =>
           {
-              List<Task< FfmpegSampleUsageRenderVideo60Seconds.SampleResult >> resultAsync = new List<Task<FfmpegSampleUsageRenderVideo60Seconds.SampleResult>>();
+              List<Task< FfmpegSampleUsageRenderImagesToVideo.SampleResult >> resultAsync = new List<Task<FfmpegSampleUsageRenderImagesToVideo.SampleResult>>();
               for (var i = 0; i < 1; i++)
               {
-                  resultAsync.Add(Task.Run(() => new FfmpegSampleUsageRenderVideo60Seconds().Convert()));
+                  resultAsync.Add(Task.Run(() => new FfmpegSampleUsageRenderImagesToVideo().Convert()));
 
               }
               var result = await Task.WhenAll(resultAsync);
-              Console.WriteLine($"Total in miliseconds: {result.Sum(i => i.ConvertResult.ConvertInMiliseconds)}");
+              Console.WriteLine($"Total in miliseconds: {result.Sum(i => i.ConvertResult.ConvertInMiliseconds) + result.Sum(i=>i.ConvertResult.SubResult.Select(i=>i.ConvertInMiliseconds).Sum()) }");
 
               foreach (var r in result)
               {
@@ -40,62 +38,6 @@ namespace Ffmpeg.UnitTestConsole
                 }
             }
 
-        }
-    }
-
-    public class FfmpegSampleUsageRenderVideo60Seconds
-    {
-        Random _rnd = new Random();
-        public FfmpegSampleUsageRenderVideo60Seconds()
-        {
-
-        }
-        public List<string> ListImageFile()
-        {
-            return Directory.GetFiles(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "ImageTest/imgs"))
-                .Select(i => i).ToList();
-        }
-
-        public List<string> ListAudioFile()
-        {
-            return Directory.GetFiles(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "ImageTest/audio"))
-                .Select(i => i).ToList();
-        }
-
-        public SampleResult Convert()
-        {
-            List<string> audios = ListAudioFile();
-            string audioFile = audios[_rnd.Next(0, audios.Count - 1)];
-
-            var dir = Path.Combine(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "ImageTest/results"));
-
-            if (!Directory.Exists(dir)) Directory.CreateDirectory(dir);
-
-            string fileOutput = Path.Combine(dir, $"video_{DateTime.Now.Ticks}.mp4");
-
-            var cmd = new FFmpegCommandBuilder().WithFileAudio(audioFile)
-                .AddFileInput(ListImageFile().Take(30).ToArray())
-                .WithFileOutput(fileOutput)
-                .WithVideoDurationInSeconds(60)
-                .WithFadeDurationInSeconds(1)
-                .ToCommand();
-
-            Console.WriteLine(fileOutput);
-
-            var r = new FfmpegCommandExecuter().Run(cmd);
-
-            return new SampleResult
-            {
-                ConvertResult = r,
-                FileVideo = fileOutput
-            };
-        }
-
-        public class SampleResult
-        {
-            public string FileVideo { get; set; }
-
-            public FfmpegCommandResult ConvertResult { get; set; }
         }
     }
 }
